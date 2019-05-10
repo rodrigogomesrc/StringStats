006ba171b2c89f75b5da5f083b4df143b4e2b308
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2019-05-10 17:40:50 -0300

    complete inicial functions

--- wordstats.py
+++ wordstats.py
@@ -34,7 +34,7 @@ class Wordstats(object):
 	def __change_characters(self, text):
 
 		"""
-		Receives a string, remove special characters and separete words into tuples
+		Receives a string, remove special characters, separate the words and put them inside an array
 		
 		"""
 
@@ -52,13 +52,14 @@ class Wordstats(object):
 
 				new_text += character
 
-		#turn the text into a tuple of words
+		#turn the text into a array of words
 
 		new_text = new_text.split()
+
 		return new_text
 	
 
-	def __array_stringfy(self, text):
+	def __array_stringfy(self, array):
 
 		"""
 		Receives a array of strings and make them into one string
@@ -67,9 +68,9 @@ class Wordstats(object):
 
 		temp_text = ""
 
-		for n in range(len(text)):
+		for n in range(len(array)):
 
-			temp_text += text[n];
+			temp_text += array[n];
 
 		new_text = temp_text
 		return new_text
@@ -109,14 +110,52 @@ class Wordstats(object):
 		file.close()
 		return frequency
 		
-	def words_frequency(self,words_range):
+	def words_frequency(self,file_path, words_range):
 
 		"""
 		Returns the frequency of every word ordened by the frequency. the argument "words_range" limits how mahy words is shown.
 
 		"""
 
-		pass
+		self.__test_file(file_path)
+		file = open(file_path, 'r')
+		text = file.readlines()
+		
+		#turn it into one string
+		text = self.__array_stringfy(text)
+		
+		#turn it into an array of words
+		text = self.__change_characters(text)
+	
+		frequency = {}
+		sorted_frequency = []
+		line = []
+
+		#count how many times a word appear
+
+		for word in text:
+			
+			if word in frequency:
+
+				frequency[word] = frequency[word] + 1
+			else:
+
+				frequency[word] = 1
+				
+                #Sort the dicionary by the value and turn it into a matrix
+	
+		for word in sorted(frequency, key=frequency.get, reverse=True):
+
+			line.append(word)
+			line.append(frequency[word])
+			sorted_frequency.append(line)
+			line = []
+
+		file.close()
+
+		return sorted_frequency
+		
+		
 
 	def words_count(self, file_path):
 
